4f33e790e9903251b89eeabd0052d06e1996773a
Troi Hicks <88952205+TroixHicks@users.noreply.github.com>
2021-09-24 21:54:57 -0400

    Add files via upload

---
+++ helloname.js
@@ -0,0 +1,13 @@
+
+function hello(name) {
+//   const convNames = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+  if (name == "johN") {
+    return "Hello, John!";
+  } else if (name == "alice") {
+    return "Hello, Alice!";
+  } else {
+    return "Hello, World!"
+  }
+}
+
+
