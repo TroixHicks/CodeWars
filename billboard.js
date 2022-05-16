cbeeb26011ccad0b11b36e50255c5187a71973cb
Troi Hicks <88952205+TroixHicks@users.noreply.github.com>
2022-05-16 09:30:45 -0400

    idk

---
+++ billboard.js
@@ -0,0 +1,9 @@
+function billboard(name, price = 30){
+
+  let totalCost= 0
+  for(i=0; i < name.length; i++){
+    totalCost += price
+  }
+
+  return totalCost;
+} 
--- multiply.js
+++ multiply.js
@@ -1,3 +1,12 @@
 function multiply(a, b){
   return a * b
 }
+
+myString.length
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++ ){
+  console.log('I would love to visit ' + vacationSpots[i]);
+}
